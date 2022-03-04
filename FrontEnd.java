209688014113233c25bf1b1b9b4e2aed64db3119
David Watt <david.watt@ontariotechu.net>
2022-03-03 21:58:50 -0500

    Added create and delete methods

--- FrontEnd.java
+++ FrontEnd.java
@@ -1,8 +1,13 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 public class FrontEnd {
+    public static Map<String, User> accountMap = new HashMap<>();
     public static void login(){
         
     }
@@ -11,8 +16,8 @@ public class FrontEnd {
     }
 
     public static void create() throws IOException{
-        BufferedReader reader = new BufferedReader(
-            new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
         String name;
         String password;
         String userType;
@@ -22,21 +27,32 @@ public class FrontEnd {
         password = reader.readLine();
         System.out.println("Please enter a user type");
         userType = reader.readLine();
-        
+        accountMap.put(name, new User(name, password, userType));
         try {
-            FileWriter myWriter = new FileWriter("dailyTransactions.txt");
-            myWriter.write("A New " + userType + " user by the name of " + name + "was created. The password is " + password);
-            myWriter.close();
+            BufferedWriter fw = new BufferedWriter(new FileWriter("dailyTransactions.txt", true));
+            fw.write("A New " + userType + " user by the name of " + name + " was created. The password is " + password);
+            fw.newLine();
+            fw.close();
           } catch (IOException e) {
             e.printStackTrace();
           }
-
+        System.out.println(accountMap);
           
     }
 
-    public static void delete(){
-
+    public static void delete() throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String usertoDelete;
+        System.out.println("Please enter a username");
+        usertoDelete = reader.readLine();
+        accountMap.remove(usertoDelete);
+        BufferedWriter fw = new BufferedWriter(new FileWriter("dailyTransactions.txt", true));
+        fw.write("User " + usertoDelete +  " was deleted");
+        fw.newLine();
+        fw.close();
+        System.out.println(accountMap);
     }
+
     public static void post(){
         
     }
@@ -50,8 +66,19 @@ public class FrontEnd {
     
 
     public static void main(String[] args) throws IOException {
- 
-
+        BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in));
+        String choice;
+        do{
+            System.out.println("Please enter option");
+            choice = reader.readLine();
+            if (choice.equals("create")){
+                create();
+            }
+            else if (choice.equals("delete")){
+                delete();
+            }
+        }while(!choice.equals("exit"));
 
     }
 
