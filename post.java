ec01d79bdaf65c4d3d37f0f0f65402b5265947d5
Nathaniel Armogan <54459997+devMRLT@users.noreply.github.com>
2022-03-05 13:03:32 -0500

    Add files via upload

---
+++ post.java
@@ -0,0 +1,26 @@
+public class Post{
+    public String cityName;
+    public double rentalPrice;
+    public int numbedrooms;
+    public boolean rentedFlag = false;
+        
+    public Post(String cityName, double rentalPrice, int numbedrooms, boolean rentedFlag){
+        this.cityName = cityName;
+        this.rentalPrice = rentalPrice;
+        this.numbedrooms = bednumbedroomsrooms;
+        this.rentedFlag = rentedFlag;
+    }
+    
+    public String getCityName(){
+        return cityName;
+    }
+    public double getrentalPrice(){
+        return rentalPrice;
+    }
+    public int getNumBedrooms(){
+        return numbedrooms;
+    }
+    public int getrentedFlag(){
+        return numbedrooms;
+    }
+    }
