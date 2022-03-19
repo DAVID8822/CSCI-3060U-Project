3a35c513a578d7145847785f27296721e571d15b
Nathaniel Armogan <54459997+devMRLT@users.noreply.github.com>
2022-03-19 13:27:13 -0400

    Update searchTest.sh
    Trying to add functionality to be able to grab the amount of files in the directory and loop through and run the input file.

--- searchTest.sh
+++ searchTest.sh
@@ -1,6 +1,9 @@
 # Start of the testing script for search
 #!/bin/bash
 chdir inputs
-foreach i(*) echo "running test $i"
-    Input/search_In.txt
+# ls -l | wc -l = size
+END= ls -l | wc -l
+for ((i=1;i<=END;i++)); do
+    Inputs/(*i)
+done
 end
