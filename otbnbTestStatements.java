f453642cf8142ba658dc32ae5959fd209b8800ed
Nathaniel Armogan <54459997+devMRLT@users.noreply.github.com>
2022-04-04 09:58:46 -0400

    Test Statements started
    I got the templates for the functions started for each statement.

---
+++ otbnbTestStatements.java
@@ -0,0 +1,120 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class 257 {
+
+	@Test
+	void testStatementLine66Test1() {
+		System.out.println("pass");
+	}
+	@Test
+	void testStatementLine66Test2() {
+		System.out.println("negative");
+	}
+	@Test
+	void testStatementLine67Test1() {
+		System.out.println("pass");
+	}
+	@Test
+	void testStatementLine67Test2() {
+		System.out.println("negative");
+	}
+	@Test
+	void testStatementLine87Test1() {
+		System.out.println("pass");
+	}
+	@Test
+	void testStatementLine87Test2() {
+		System.out.println("negative");
+	}
+	@Test
+	void testStatementLine210Test1() {
+		System.out.println("pass");
+	}
+	@Test
+	void testStatementLine210Test2() {
+		System.out.println("negative");
+	}
+	@Test
+	void testStatementLine235Test1() {
+		System.out.println("pass");
+	}
+	@Test
+	void testStatementLine235Test2() {
+		System.out.println("negative");
+	}
+	@Test
+	void testStatementLine249Test1() {
+		System.out.println("pass");
+	}
+	@Test
+	void testStatementLine249Test2() {
+		System.out.println("negative");
+	}
+	@Test
+	void testStatementLine249Test1() {
+		System.out.println("pass");
+	}
+	@Test
+	void testStatementLine249Test2() {
+		System.out.println("negative");
+	}
+	@Test
+	void testStatementLine257Test1() {
+		System.out.println("pass");
+	}
+	@Test
+	void testStatementLine257Test2() {
+		System.out.println("negative");
+	}
+	@Test
+	void testStatementLine268Test1() {
+		System.out.println("pass");
+	}
+	@Test
+	void testStatementLine268Test2() {
+		System.out.println("negative");
+	}
+	@Test
+	void testStatementLine273Test1() {
+		System.out.println("pass");
+	}
+	@Test
+	void testStatementLine273Test2() {
+		System.out.println("negative");
+	}
+	@Test
+	void testStatementLine313Test1() {
+		System.out.println("pass");
+	}
+	@Test
+	void testStatementLine313Test2() {
+		System.out.println("negative");
+	}
+	@Test
+	void testStatementLine346Test1() {
+		System.out.println("pass");
+	}
+	@Test
+	void testStatementLine346Test2() {
+		System.out.println("negative");
+	}
+	@Test
+	void testStatementLine352Test1() {
+		System.out.println("pass");
+	}
+	@Test
+	void testStatementLine352Test2() {
+		System.out.println("negative");
+	}
+	@Test
+	void testStatementLine352Test1() {
+		System.out.println("pass");
+	}
+	@Test
+	void testStatementLine352Test2() {
+		System.out.println("negative");
+	}
+
+}
