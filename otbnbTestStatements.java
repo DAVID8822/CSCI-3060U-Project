ce7bedafab0b0da1dd0a4335e7c4774b2f758155
Nathaniel Armogan <54459997+devMRLT@users.noreply.github.com>
2022-04-05 16:02:03 -0400

    Update otbnbTestStatements.java
    templates setup

--- otbnbTestStatements.java
+++ otbnbTestStatements.java
@@ -3,9 +3,185 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class 257 {
+
 	@Test
-	void logoutTest() {
-		System.out.println("testing the output of the logout");
-		Assertions.assertEquals(currentUser.name, currentUser.userType, outputFile, storedOutput);
+	void testStatementLine66Test1() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine66Test2() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine67Test1() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine67Test2() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine87Test1() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine87Test2() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine210Test1() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine210Test2() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine235Test1() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine235Test2() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine249Test1() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine249Test2() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine249Test1() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine249Test2() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine257Test1() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine257Test2() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine268Test1() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine268Test2() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine273Test1() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine273Test2() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine313Test1() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine313Test2() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine346Test1() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine346Test2() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine352Test1() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine352Test2() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine352Test1() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine352Test2() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine415Test2() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine421Test2() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine427Test2() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine430Test2() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine433Test2() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine438Test2() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine441Test2() {
+		System.out.println("");
+		Assertions.assertEquals();
+	}
+	@Test
+	void testStatementLine446Test2() {
+		System.out.println("");
+		Assertions.assertEquals();
 	}
 }
